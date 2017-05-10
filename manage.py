821a315ce446bd96e20d4e0c86d6c1bf685b2516
Bill Zografos <billzografos96@yahoo.com>
2017-05-10 16:57:22 +0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ur8_proj.settings")
-    try:
+    try:abc
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
